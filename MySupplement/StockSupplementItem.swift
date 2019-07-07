1308bd8ef9bcee4cd12cf3b1fbecda1972274655
hmym <chika.hamayama@gmail.com>
2019-07-07 18:28:45 +0900

    コメント追加、クラス追加、レイアウト修正

---
+++ MySupplement/StockSupplementItem.swift
@@ -0,0 +1,14 @@
+//
+//  StockSupplementItem.swift
+//  MySupplement
+//
+//  Created by 濱山知香 on 2019/06/29.
+//  Copyright © 2019 濱山知香. All rights reserved.
+//
+
+import Foundation
+
+class StockSupplementItem {
+    let titles = ["ビタミンB", "ビタミンC", "ビタミンE"]
+    let images = ["B-50", "C-1000", "E-1000"]
+}
--- MySupplement/ViewController.swift
+++ MySupplement/ViewController.swift
@@ -7,25 +7,32 @@
 //
 
 import UIKit
-
+//UITableViewDelegate, UITableViewDataSourceのプロトコルを実装する旨の宣言を行う
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+    // StockSupplementItemクラスをitemに代入
+    let item = StockSupplementItem()
+    // tableViewを関連付け
     @IBOutlet weak var stockSupplementList: UITableView!
-
+    // addボタンをタップした時に呼ばれる処理
     @IBAction func addStockItem(_ sender: Any) {
+        // Storybordで指定したaddStockItemをaddStockItemViewControllerに代入
         let addStockItemViewController = storyboard!.instantiateViewController(withIdentifier: "addStockItem")
+        // モーダルでaddStockItemViewControllerが出現
         self.present(addStockItemViewController, animated: true, completion: nil)
     }
-    
+    // テーブルの行数を返す
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
-    
+    // テーブルの行ごとのセルを返す
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Storybordで指定したstockSupplementCell識別子を利用して再利用可能なセルを取得する
         let cell = stockSupplementList.dequeueReusableCell(withIdentifier: "stockSupplementCell") as! StockSupplementCell
-        cell.supplementThumdnail.image = UIImage(named: "ship")
-        cell.supplementName.text = "ビタミンA"
-        
+        // セルのimageに商品画像をセット
+        cell.supplementThumdnail.image = UIImage(named: item.images[indexPath.row])
+        // セルのラベルに商品タイトルをセット
+        cell.supplementName.text = item.titles[indexPath.row]
+    
         return cell
     }
     
